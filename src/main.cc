1ccdfa7e7481d722ed34d4af90e7f8a4aaa0aaa5
sksat <sksat.tec@gmail.com>
2019-02-13 22:00:29 +0900

    [UPDATE] compiler,linker option

--- src/main.cc
+++ src/main.cc
@@ -1,22 +1,34 @@
 #include "multiboot2.h"
 
-#define VRAM	0xB8000
+#define VRAM		0xB8000
+#define COLUMNS		80
+#define LINES		24
+#define ATTRIBUTE	7
+
+// functions
+void cls();
+void putchar(int x, int y, int c);
+
+extern "C" void kmain(unsigned long magic, unsigned long addr){
+	cls();
+
+	putchar(0, 1, 'A');
+	putchar(1, 1, 'B');
+	putchar(2, 1, 'C');
+
+	while(1);
+	return;
+}
 
 void cls(){
 	volatile unsigned char* vram = (unsigned char*) VRAM;
-	for(int i=0;i<80*24*2;i++)
-		*(vram + i) = 1;
+	for(int i=0;i<COLUMNS*LINES*2;i++)
+		*(vram+i) = 0;
 }
 
 void putchar(int x, int y, int c){
-	unsigned char* vram = (unsigned char*) VRAM;
-	auto addr = vram + (x + y * 80)*2;
+	volatile unsigned char *vram = (unsigned char*) VRAM;
+	auto addr = vram + (x + y * COLUMNS)*2;
 	*addr = c & 0xff;
-	*(addr+1) = 7;
-}
-
-extern "C" void kmain(unsigned long magic, unsigned long addr){
-	putchar(0, 1, 'A');
-	while(1);
-	return;
+	*(addr+1) = ATTRIBUTE;
 }
